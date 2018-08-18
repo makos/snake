272020c78e115a6daf555e445c7a7fd3d1490508
Mateusz Makowski <matmakos@gmail.com>
2018-08-18 20:45:47 +0200

    Add function declarations to game.h

--- include/game.h
+++ include/game.h
@@ -4,6 +4,7 @@
 #include <curses.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 // Needs "utils.h" "apple.h" "player.h"
 typedef struct Vec Vec_t;
@@ -27,9 +28,19 @@ const Vec_t DIR_DOWN;
 const short RED, GREEN, YELLOW;
 // Global is the game running? boolean.
 bool RUN;
+// Game speed.
+int SPEED;
 
 void reset_game(Snake_t *player);
 void game_over(Snake_t *player);
 void new_game_prompt();
+void setup_colors();
+void game_setup();
+//DEBUG:
+void debug_info(WINDOW *win, Snake_t *player);
+void check_valid_move(Snake_t *player, Vec_t dir);
+void handle_input(Snake_t *player, int ch);
+void set_defaults();
+void parse_args(int argc, char *argv[]);
 
 #endif //GAME_H
