41b725684be20f92605c0330216d5c96d5165445
Mateusz Makowski <matmakos@gmail.com>
2018-08-18 12:48:22 +0200

    Add incomplete random_vec() utility

--- src/utils.c
+++ src/utils.c
@@ -1,16 +1,19 @@
 #include "game.h"
 #include "utils.h"
 
+/* Tests equality between two Vec_t points. */
 int is_eq(Vec_t dir, Vec_t other)
 {
     return dir.y == other.y && dir.x == other.x;
 }
 
-// WINDOW *create_win(int height, int width, int starty, int startx)
-// {
-//     WINDOW *local;
-//     local = newwin(height, width, starty, startx);
-//     box(local, 0, 0);
-//     // wrefresh(local);
-//     return local;
-// }
+//TODO: Make this actually RNG
+Vec_t random_vec()
+{
+    time_t tt;
+    srand((time(&tt) * 377) % 23);
+    int rand_y = rand() % LINES;
+    int rand_x = rand() % COLS;
+    Vec_t vec = {rand_y, rand_x};
+    return vec;
+}
