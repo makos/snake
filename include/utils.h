38a858bd7c2f921976ab7c1fb9af900c980515af
Mateusz Makowski <matmakos@gmail.com>
2018-08-18 12:48:50 +0200

    random_vec()

--- include/utils.h
+++ include/utils.h
@@ -1,9 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 typedef struct Vec Vec_t;
-// typedef struct _win WINDOW;
 
 int is_eq(Vec_t dir, Vec_t other);
-// WINDOW *create_win(int height, int width, int starty, int startx);
+Vec_t random_vec();
 
 #endif //UTILS_H
