bd970c8d31c448c5d53fc978771c4b0f03ab3717
Mateusz Makowski <matmakos@gmail.com>
2018-08-18 12:48:32 +0200

    Implement random_vec()

--- src/apple.c
+++ src/apple.c
@@ -1,16 +1,18 @@
 #include "game.h"
 #include "apple.h"
+#include "utils.h"
 
 Apple_t *new_random_apple()
 {
     // Initialize the RNG with current CPU clock time
-    time_t tt;
-    srand(time(&tt));
-    int rand_y = rand() % LINES;
-    int rand_x = rand() % COLS;
+    // time_t tt;
+    // srand(time(&tt));
+    // int rand_y = rand() % LINES;
+    // int rand_x = rand() % COLS;
     Apple_t *apple = (Apple_t *)malloc(sizeof(Apple_t));
-    apple->pos.y = rand_y;
-    apple->pos.x = rand_x;
+    apple->pos = random_vec();
+    // apple->pos.y = rand_y;
+    // apple->pos.x = rand_x;
     apple->ch = '@';
     return apple;
 }
